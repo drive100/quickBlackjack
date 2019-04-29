5b12a10016ce3ee1df123c511117df5025366734
Daniel Rivera <drive100@syr.edu>
2019-04-28 22:10:43 -0400

    files

--- quickBlackjack/GameViewController.swift
+++ quickBlackjack/GameViewController.swift
@@ -11,10 +11,24 @@ import UIKit
 class GameViewController: UIViewController {
 
     
+    @IBOutlet weak var topRight: UIImageView!
+    @IBOutlet weak var topMiddle: UIImageView!
+    @IBOutlet weak var topLeft: UIImageView!
+    @IBOutlet weak var bottomRight: UIImageView!
+    @IBOutlet weak var bottomLeft: UIImageView!
+    @IBOutlet weak var bottomMiddle: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        bottomLeft.image =  UIImage(named:"png/blank")
+        bottomMiddle.image = UIImage(named:"png/blank")
+        bottomRight.image = UIImage(named:"png/blank")
+        
+        topLeft.image = UIImage(named:"png/blank")
+        topMiddle.image = UIImage(named:"png/blank")
+        topRight.image = UIImage(named:"png/blank")
     }
     
    
