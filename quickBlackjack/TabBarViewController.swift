d13fffd59fa28ad4eef4a29b66c0459ec443f79b
Daniel Rivera <drive100@syr.edu>
2019-05-01 00:46:26 -0400

    added tabbarcontroller

---
+++ quickBlackjack/TabBarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TabBarViewController.swift
+//  quickBlackjack
+//
+//  Created by Danny Rivera on 5/1/19.
+//  Copyright © 2019 Danny Rivera. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController:  UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.selectedIndex = 1
+
+    }
+
+}
